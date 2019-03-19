80cf99dbb4b1f0101a6521e3175a85a32146c271
Jonas <jonas@tipser.com>
2019-03-20 00:09:05 +0100

    [Initial] Initial commit for the Tipser Elements Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React, { Component } from 'react';
+import { TipserElement, TipserProduct, CartIcon, TipserContextProvider } from '@tipser/tipser-elements';
 import logo from './logo.svg';
 import './App.css';
+
+
+let tipserConfig = {
+    posId: '59e86b79b8f3f60a94ecd26a',
+    lang: 'en',
+    env: 'prod',
+    primaryColor: 'red'
+};
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <TipserContextProvider value = { tipserConfig }>   
+          <div>
+              <header className="header">
+                  <nav className="navigation">
+                      <ul className="horizontal-menu">
+                          <li className="horizontal-item">
+                              <img src={logo} className="te-logo" alt="logo" />
+                          </li>
+                          <li className="horizontal-item">
+                              <a
+                                  className="link"
+                                  href="https://developers.tipser.com"
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                              >
+                                  Tipser Developers
+                              </a>
+                          </li>
+                          <li className="horizontal-item">
+                              <a
+                                  className="link"
+                                  href="https://tipser.github.io/docs/#tipser-elements"
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                              >
+                                  Tipser Elements documentation
+                              </a>
+                          </li>
+                          <li className="horizontal-item te-cart">
+                              {/* Inserting the Tipser cart Icon */}
+                              <CartIcon/>
+                          </li>
+                      </ul>
+                  </nav>
+              </header>
+              <main>
+                  {/* Inserting an elment from Contentful taking the content Id as the prop. This example renders a page, consisting of other components */}
+                  <TipserElement  id="2OeW1Ab8lKNcUq6dVUh79" />
+
+                  {/* Inserting a product using the productId as prop  */}
+                  <TipserProduct productId="5ba2334a781baa0001ccdffc"/>
+
+                  {/* Inserting an elment from Contentful taking the content Id as the prop. This example renders a page, consisting of other components */}
+                  <TipserElement  id="1larHZb8TeMQiqmi4W8CIS" />
+              </main>
+              {/* Inserting a custom footer */}
+              <footer>
+                  <span className="footer-text">This is the footer</span>
+              </footer>
+          </div>
+      </TipserContextProvider>
     );
   }
 }
