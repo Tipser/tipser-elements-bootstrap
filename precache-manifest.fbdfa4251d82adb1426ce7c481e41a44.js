a5c78993880f3a2b9387cb6c79966391ae08721e
Piotr Sobczyk <sobczyk.peter@gmail.com>
2019-04-03 16:52:18 +0200

    Updates

---
+++ precache-manifest.fbdfa4251d82adb1426ce7c481e41a44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a237c37ba031a419fac1",
+    "url": "/tipser-elements-bootstrap/static/css/main.d92a1e98.chunk.css"
+  },
+  {
+    "revision": "a237c37ba031a419fac1",
+    "url": "/tipser-elements-bootstrap/static/js/main.83b9b145.chunk.js"
+  },
+  {
+    "revision": "e472c5bc35479d6348a2",
+    "url": "/tipser-elements-bootstrap/static/js/runtime~main.46ff05ed.js"
+  },
+  {
+    "revision": "4106f9c4b1a0a7c8de0d",
+    "url": "/tipser-elements-bootstrap/static/js/2.b865dd5e.chunk.js"
+  },
+  {
+    "revision": "037149093731b28e692da4b6859da42c",
+    "url": "/tipser-elements-bootstrap/static/media/logo.03714909.svg"
+  },
+  {
+    "revision": "6499c2bde2b6607bfdd60036c548e716",
+    "url": "/tipser-elements-bootstrap/index.html"
+  }
+];
