159e857c0c075bfde0fb47cfdd6d5ca28afd1915
Piotr Sobczyk <sobczyk.peter@gmail.com>
2019-04-04 14:50:23 +0200

    Added examples of new collections to the main page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { TipserElement, TipserProduct, CartIcon, TipserElementsProvider } from '@tipser/tipser-elements';
+import { TipserElement, TipserProduct, CartIcon, TipserElementsProvider, CollectionComponent } from '@tipser/tipser-elements';
 import logo from './logo.svg';
 import './App.css';
 
@@ -59,6 +59,10 @@ class App extends Component {
 
                   {/* Inserting an elment from Contentful taking the content Id as the prop. This example renders a page, consisting of other components */}
                   <TipserElement  id="1larHZb8TeMQiqmi4W8CIS" />
+
+                  <CollectionComponent collectionId="5beaaa0653e83f0001e18577" />
+
+                  <CollectionComponent collectionId="5bec3f0e8d1736000154335a" />
               </main>
               {/* Inserting a custom footer */}
               <footer>
