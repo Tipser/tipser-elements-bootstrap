264cdf60949080994e61e1a25c50483dea222d4e
Maciej Guzek <maciej@tipser.com>
2019-04-12 07:22:38 +0200

    T3-2790 Similar products

--- src/views/product-view.js
+++ src/views/product-view.js
@@ -1,16 +1,28 @@
 import * as React from 'react';
-import { TipserElement, TipserProduct } from '@tipser/tipser-elements';
+import { connect, PromiseState } from 'react-refetch'
+
+import { TipserElement, TipserProduct, TipserProductComponent, CollectionComponent } from '@tipser/tipser-elements';
 import { Header } from '../components/header';
 
-export class ProductView extends React.Component {
+class ProductViewComponent extends React.Component {
 
     render() {
         const { productId } = this.props.match.params;
+        const { similarProductsFetch } = this.props;
+
         return <React.Fragment>
             <Header></Header>
             <main>
             <TipserProduct productId={productId} />
             </main>
+            <h2>Similar products</h2>
+            {similarProductsFetch.fulfilled && <div>
+                <CollectionComponent products={similarProductsFetch.value}/>
+            </div>}
         </React.Fragment>
     }
 }
+
+export const ProductView = connect(props => ({
+    similarProductsFetch: `https://t3-prod-api.tipser.com/v3/products/${props.match.params.productId}/similar?onlyAvailable=true`
+  }))(ProductViewComponent)
