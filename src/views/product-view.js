e58cd20b40dd0d47153d5b0d5190b5fa6aa4fe0e
Maciej Guzek <maciej@tipser.com>
2019-04-12 06:59:49 +0200

    T3-2790 Added product id from url to tipser element product

--- src/views/product-view.js
+++ src/views/product-view.js
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import { TipserElement, TipserProduct } from '@tipser/tipser-elements';
+import { Header } from '../components/header';
 
 export class ProductView extends React.Component {
 
     render() {
         const { productId } = this.props.match.params;
         return <React.Fragment>
-            xxxxxxxx - {productId}
+            <Header></Header>
+            <main>
+            <TipserProduct productId={productId} />
+            </main>
         </React.Fragment>
     }
 }
