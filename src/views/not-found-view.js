99633970dbde86b427046aca793d575783902795
Maciej Guzek <maciej@tipser.com>
2019-04-12 07:31:30 +0200

    T3-2790 removed unused const

--- src/views/not-found-view.js
+++ src/views/not-found-view.js
@@ -3,7 +3,6 @@ import * as React from 'react';
 export class NotFoundView extends React.Component {
 
     render() {
-        const { productId } = this.props;
         return <React.Fragment>
             The page is not found
         </React.Fragment>
