48a7771d97ce74c806e9cb4e18b1cd0833ffe8fc
Jenkins <jenkins@tipser.com>
2019-06-06 08:17:07 +0000

    Updates

---
+++ precache-manifest.bc6bed0bd9b67fc4d08b8e6d6f8bf056.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "037149093731b28e692da4b6859da42c",
+    "url": "/tipser-elements-react-bootstrap/static/media/logo.03714909.svg"
+  },
+  {
+    "revision": "9b8134df620242f02a01",
+    "url": "/tipser-elements-react-bootstrap/static/js/runtime~main.e2fe5a93.js"
+  },
+  {
+    "revision": "110efde9e0174b4464e6",
+    "url": "/tipser-elements-react-bootstrap/static/js/main.d0365938.chunk.js"
+  },
+  {
+    "revision": "cd2bc9850fd77fdabf63",
+    "url": "/tipser-elements-react-bootstrap/static/js/2.cc8af5df.chunk.js"
+  },
+  {
+    "revision": "110efde9e0174b4464e6",
+    "url": "/tipser-elements-react-bootstrap/static/css/main.3a0185d8.chunk.css"
+  },
+  {
+    "revision": "87ca997f8fae381b7da5c30962dc3963",
+    "url": "/tipser-elements-react-bootstrap/index.html"
+  }
+];
