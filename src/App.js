d5bb92e99099a09a2705f49fad150a41630bbbd4
Mateusz Sikora <ms1qaz@gmail.com>
2019-08-19 19:32:37 +0200

    bool flag instead of sentry env

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
     render() {
         return (
             <Router history={hashHistory}>
-                <TipserElementsProvider posId={'59f1ef63b8f3f6727cef1d34'} config={tipserConfig} sentryEnv="frontend_dev">
+                <TipserElementsProvider posId={'59f1ef63b8f3f6727cef1d34'} config={tipserConfig} isSentryEnabled={false}>
                     <Switch>
                         <Route path="/product/:productId" component={ProductView} />
                         <Route path="/page/:pageId" component={PageView} />
