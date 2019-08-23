dff14e612522f04d6d1d5377a3c53cb282f46245
Wojciech Piątkowski <chmurson@gmail.com>
2019-08-23 10:45:47 +0200

    switch to stage version of tipser.com

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
 
 let tipserConfig = {
     lang: 'en',
+    env: 'stage',
     primaryColor: 'red',
     useDefaultErrorHandler: true,
     eventsHandlers: {
