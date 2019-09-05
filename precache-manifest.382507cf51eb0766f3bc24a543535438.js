605bb35ce5026f84970ffa527473be3c30f61c4c
AgaK-O <hembrita@op.pl>
2019-09-05 09:29:23 +0200

    Updates

---
+++ precache-manifest.382507cf51eb0766f3bc24a543535438.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "037149093731b28e692da4b6859da42c",
+    "url": "/tipser-elements-react-bootstrap/static/media/logo.03714909.svg"
+  },
+  {
+    "revision": "9b8134df620242f02a01",
+    "url": "/tipser-elements-react-bootstrap/static/js/runtime~main.e2fe5a93.js"
+  },
+  {
+    "revision": "a92ec256273ad17e163e",
+    "url": "/tipser-elements-react-bootstrap/static/js/main.e6c6f69f.chunk.js"
+  },
+  {
+    "revision": "4b2a807eac91fa1b0e12",
+    "url": "/tipser-elements-react-bootstrap/static/js/2.f6dbbd25.chunk.js"
+  },
+  {
+    "revision": "a92ec256273ad17e163e",
+    "url": "/tipser-elements-react-bootstrap/static/css/main.213df642.chunk.css"
+  },
+  {
+    "revision": "9f0384b7ccdc9f38d7791c6f0f492cdf",
+    "url": "/tipser-elements-react-bootstrap/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tipser-elements-react-bootstrap/precache-manifest.af7b9d4e32350d1579afffe2a3e22027.js"
+  "/tipser-elements-react-bootstrap/precache-manifest.382507cf51eb0766f3bc24a543535438.js"
 );
 
 workbox.clientsClaim();
