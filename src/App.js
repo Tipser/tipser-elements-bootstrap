041129c8b2cd6919c934576a2e9a6fecd7a117a9
Mateusz Sikora <ms1qaz@gmail.com>
2019-12-19 12:33:01 +0100

    adjust version of react router to the version which is required by tipser-elements. remove unused code and deps

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import { Route, Switch, Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Switch } from 'react-router';
+import { createHashHistory } from 'history';
 import { TipserElementsProvider } from '@tipser/tipser-elements';
+
 import { ComponentsView } from './views/components-view';
 import { ProductView } from './views/product-view';
 import { NotFoundView } from './views/not-found-view';
-import './App.css';
-import { createHashHistory } from 'history';
 import { PageView } from './views/page-view';
 
 import '@tipser/tipser-elements/dist/index.css';
+import './App.css';
+
 
 const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
 
@@ -31,16 +34,16 @@ let tipserConfig = {
 class App extends Component {
     render() {
         return (
-            <Router history={hashHistory}>
-                <TipserElementsProvider posId={'59e86b79b8f3f60a94ecd26a'} config={tipserConfig} sentryEnv="frontend_dev">
+            <TipserElementsProvider posId={'59e86b79b8f3f60a94ecd26a'} config={tipserConfig} sentryEnv="frontend_dev">
+                <BrowserRouter history={hashHistory}>
                     <Switch>
-                        <Route path="/product/:productId" component={ProductView} />
-                        <Route path="/page/:pageId" component={PageView} />
-                        <Route path="/" component={ComponentsView} />
-                        <Route component={NotFoundView} />
+                        <Route path="/product/:productId" component={ProductView}/>
+                        <Route path="/page/:pageId" component={PageView}/>
+                        <Route path="/" component={ComponentsView}/>
+                        <Route component={NotFoundView}/>
                     </Switch>
-                </TipserElementsProvider>
-            </Router>
+                </BrowserRouter>
+            </TipserElementsProvider>
         );
     }
 }
