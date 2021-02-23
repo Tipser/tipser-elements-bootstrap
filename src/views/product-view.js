31098d2a09db0204ee8bb4f4d214c33a2747abae
piotrek <piotr@tipser.com>
2021-02-23 11:43:23 +0100

    Converted ProductView to the functional component syntax

--- src/views/product-view.js
+++ src/views/product-view.js
@@ -4,25 +4,23 @@ import { Product } from '@tipser/tipser-elements';
 import { Header } from '../components/header';
 import { Footer } from '../components/Footer/footer';
 
-export class ProductView extends React.Component {
-    render() {
-        const { productId } = this.props.match.params;
+export const ProductView = (props) => {
+    const { productId } = props.match.params;
 
-        return (
-            <React.Fragment>
-                <Header />
-                <main>
-                    <div className="container">
-                        <h3>Product</h3>
-                        <Product productId={productId} />
-                        <h3>Product card</h3>
-                        <Product productId={productId} renderer="card" />
-                        <h3>Product Compact</h3>
-                        <Product productId={productId} viewMode="compact" />
-                    </div>
-                </main>
-                <Footer />
-            </React.Fragment>
-        );
-    }
-}
+    return (
+        <React.Fragment>
+            <Header />
+            <main>
+                <div className="container">
+                    <h3>Product</h3>
+                    <Product productId={productId} />
+                    <h3>Product card</h3>
+                    <Product productId={productId} renderer="card" />
+                    <h3>Product Compact</h3>
+                    <Product productId={productId} viewMode="compact" />
+                </div>
+            </main>
+            <Footer />
+        </React.Fragment>
+    );
+};
